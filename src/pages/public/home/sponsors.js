d89cf689829bea271df0c3e18b03c607970dfeb6
Austin Kettner <austinkettner@users.noreply.github.com>
2017-10-07 23:16:28 -0400

    Remove Quote and Source from Sponsor

--- src/pages/public/home/sponsors.js
+++ src/pages/public/home/sponsors.js
@@ -4,7 +4,7 @@ export default [
   {
     name: 'Expensify',
     logo: logoExpensifiy,
-    quote: 'In hac habitasse platea dictumst. Nullam mattis orci rhoncus justo maximus, nec viverra risus condimentum. Morbi erat magna, aliquam et risus et, bibendum fringilla dolor.',
-    source: 'Albert Einstein, CEO',
+    quote: '',
+    source: '',
   },
 ];
