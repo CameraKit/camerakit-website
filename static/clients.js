f3a7b474f60b5ca9101cff3a72180adae61383d8
Austin Kettner <austinkettner@gmail.com>
2018-08-12 16:25:18 -0400

    Add InFitting to Company List

--- static/clients.js
+++ static/clients.js
@@ -1,7 +1,13 @@
 import logoBuddy from './sp_buddy.svg';
-import logoExpensify from './sp_expensify.svg';
+import logoExpensify from './brand_expensify_logo.svg';
+import logoInfitting from './brand_infitting_logo.svg';
 
 export default [
+  {
+    name: 'InFitting',
+    logo: logoInfitting,
+    link: 'https://www.infitting.com/',
+  },
   {
     name: 'Expensify',
     logo: logoExpensify,
