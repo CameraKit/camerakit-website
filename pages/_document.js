8b14572bc653b0d8e1b535c56c3d45d948d403ab
Austin Kettner <austinkettner@users.noreply.github.com>
2018-10-08 17:52:49 -0400

    Update Meta and ESLint Config

--- pages/_document.js
+++ pages/_document.js
@@ -1,57 +1,19 @@
 // from https://github.com/markolofsen/nextjs-toolkit/blob/master/pages/_document.js
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
-import Helmet from 'react-helmet';
 
-import { GA_TRACKING_ID } from '../lib/gtag'
+import { GA_TRACKING_ID } from '../lib/gtag';
 
-class MyDocument extends Document {
-  static async getInitialProps(...args) {
-    const documentProps = await super.getInitialProps(...args);
-    // see https://github.com/nfl/react-helmet#server-usage for more information
-    // 'head' was occupied by 'renderPage().head', we cannot use it
-    return { ...documentProps, helmet: Helmet.renderStatic() };
-  }
-
-  // should render on <html>
-  get helmetHtmlAttrComponents() {
-    return this.props.helmet.htmlAttributes.toComponent();
-  }
-
-  // should render on <body>
-  get helmetBodyAttrComponents() {
-    return this.props.helmet.bodyAttributes.toComponent();
-  }
-
-  // should render on <head>
-  get helmetHeadComponents() {
-    return Object.keys(this.props.helmet)
-      .filter(el => el !== 'htmlAttributes' && el !== 'bodyAttributes')
-      .map(el => this.props.helmet[el].toComponent());
-  }
-
-
-  get helmetJsx () {
-    return (<Helmet
-      htmlAttributes={{lang: 'en'}}
-      title='CameraKit | Unified Camera Library for Android'
-      meta={[
-        { name: 'viewport', content: 'user-scalable=0, initial-scale=1, minimum-scale=1, width=device-width, height=device-height' },
-        { name: 'description', content: 'CameraKit is an open-source library to support the Android Camera 1 and 2 APIs. Massively increase stability and reliability of photo and video capture on all Android devices.' },
-        { property: 'og:title', content: 'CameraKit | Unified Camera Library for Android' }
-      ]}
-    />)
-  }
+export default class CameraKitWebsite extends Document {
 
   render() {
     return (
-      <html lang="en" dir="ltr" {...this.helmetHtmlAttrComponents}>
+      <html lang="en" dir="ltr">
         <Head>
           <title>CameraKit | Unified Camera Library for Android</title>
           <meta name="description" content="CameraKit is an open-source library to support the Android Camera 1 and 2 APIs. Massively increase stability and reliability of photo and video capture on all Android devices." />
           <meta property="og:type" content="website" />
           <meta name="viewport" content="user-scalable=0, initial-scale=1, minimum-scale=1, width=device-width, height=device-height" />
-          { this.helmetHeadComponents }
           <link rel="stylesheet" href="/_next/static/style.css" />
           <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/static/favicon/apple-touch-icon-57x57.png" />
           <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/static/favicon/apple-touch-icon-114x114.png" />
@@ -74,17 +36,19 @@ class MyDocument extends Document {
           <meta name="msapplication-wide310x150logo" content="/static/favicon/mstile-310x150.png" />
           <meta name="msapplication-square310x310logo" content="/static/favicon/mstile-310x310.png" />
           <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
-          <script dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}');
-          `}}
+          <script
+            dangerouslySetInnerHTML={{
+              __html:  `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}');
+              `
+            }}
           />
           <script src="https://js.stripe.com/v3/" />
         </Head>
-        <body style={{ margin: 0 }} {...this.helmetBodyAttrComponents}>
+        <body style={{ margin: 0 }}>
           <Main />
           <NextScript />
         </body>
@@ -92,5 +56,3 @@ class MyDocument extends Document {
     );
   }
 }
-
-export default MyDocument;
