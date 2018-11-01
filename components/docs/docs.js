07437a9ef30983295fa911794d989a5fd0c8209d
Adrian Mateoaea <adrianmateoaea24@gmail.com>
2018-11-01 11:47:15 +0200

    Initial multi-versioned docs page
    #45

--- components/docs/docs.js
+++ components/docs/docs.js
@@ -1,26 +1,67 @@
 import React from 'react';
+import showdown from 'showdown';
 
+import '../../styles/styles.global.scss';
 import styles from '../../styles/docs.scss';
 
 class Docs extends React.Component {
   constructor(props) {
     super(props);
+    
+    const converter = new showdown.Converter({ metadata: true });
+    const html = converter.makeHtml(props.docs);
 
-    this.state = {
-    };
+    const regex = /<(h[2-4]) [^>]+>(.+)<\/h[2-4]>/gi;
+    const tableOfContents = [];
+
+    const getStyle = type => {
+      switch (type) {
+        case 'h3': return styles.documentation__toc__h3;
+        case 'h4': return styles.documentation__toc__h3;
+        default: return styles.documentation__toc__h2;
+      }
+    }
+    
+    let match = regex.exec(html);
+    while(match) {
+      const idRegex = /<.*id="(.+)".*>/gi;
+      const id = idRegex.exec(match[0])[1];
+      tableOfContents.push({
+        style: getStyle(match[1]), text: match[2], id
+      });
+      match = regex.exec(html);
+    }
+
+    this.state = { html, tableOfContents };
   }
 
   render() {
+    const { html, tableOfContents } = this.state;
+    const { version } = this.props;
+
     return (
       <section className={styles.documentation}>
-        <div className={styles.welcome__message}>
-          <h1>
-            {'Welcome to our new Docs'}
-          </h1>
-          <h3>
-            {'Documentation for all version of CameraKit coming soon.'}
-          </h3>
+        <div className={styles.documentation__toc}>
+          <select
+            defaultValue={`?v=${version}`}
+            className={styles.documentation__toc__select}
+            onChange={e => {
+              const query = e.target.value;
+              window.location = query;
+            }}
+          >
+            <option value="?v=0.1.0">Version 0.1.0</option>
+            <option value="?v=0.2.0">Version 0.2.0</option>
+          </select>
+          <ul className={styles.documentation__toc__ul}>
+            {tableOfContents.map((item, index) =>
+              <li key={index} className={item.style}>
+                <a href={`#${item.id}`} dangerouslySetInnerHTML={{ __html: item.text }} />
+              </li>
+            )}
+          </ul>
         </div>
+        <div style={{ flex: 1 }} dangerouslySetInnerHTML={{ __html: html }} />
       </section>
     );
   }
--- pages/docs.js
+++ pages/docs.js
@@ -4,12 +4,30 @@ import Nav from '../components/navbar';
 import Docs from '../components/docs/docs';
 import Footer from '../components/footer';
 
-const Contact = () => (
-  <div className="public">
-    <Nav />
-    <Docs />
-    <Footer />
-  </div>
-);
+class Contact extends React.Component {
+  static async getInitialProps({ query }) {
+    let docs;
+
+    const version = query.v || '0.1.0';
+    
+    if (!process.browser) {
+      const fs = require('fs');
+      docs = fs.readFileSync(`./components/docs/versions/${version}.md`, 'utf8');
+    }
+
+    return { docs, version };
+  }
+
+  render() {
+    const { docs, version } = this.props;
+    return (
+      <div className="public">
+        <Nav />
+        <Docs docs={docs} version={version} />
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default Contact;
