df21331b44b9d401c43d1b0088eae2c67bf1f181
Adrian Mateoaea <adrianmateoaea24@gmail.com>
2018-11-04 15:00:39 +0200

    Make blog page structure inline with the rest of the pages

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-import Nav from '../components/navbar';
-import Blog from '../components/blog';
+import Page from '../components/page';
+import Header from '../components/header';
+import Navbar from '../components/navbar';
 import Footer from '../components/footer';
+import Blog from '../components/blog';
+import SocialMeta from '../components/social-meta';
+import { MediaQueryConsumer } from '../components/media-query';
 
 import * as Feed from '../lib/feed';
 
@@ -21,13 +25,24 @@ class BlogPage extends React.Component {
 
   render() {
     const { feed } = this.props;
-
     return (
-      <div className="public">
-        <Nav />
+      <Page title="CameraKit | Blog">
+        <MediaQueryConsumer>
+          {({ isMobile }) => (
+            <Header
+              height={0}
+              offset={0}
+              distance={32}
+              shadow
+              active={isMobile ? 32 : 320}
+            >
+              <Navbar />
+            </Header>
+          )}
+        </MediaQueryConsumer>
         <Blog feed={feed.items} />
         <Footer />
-      </div>
+      </Page>
     );
   }
 }
